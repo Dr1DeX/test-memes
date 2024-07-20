f4fea6bcefef300f1ef5db0d3d287289ad5f55b2
nesterov-art <nesterov-art@yandex-team.ru>
2024-07-20 17:02:38 +0300

    feature: (Backend) Add CORSMiddleware

--- app/posts/main.py
+++ app/posts/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.posts.handlers import router as posts_router
 
 app = FastAPI(title='Сервис со смешнявками :3')
 
 app.include_router(posts_router)
+
+
+origins = [
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
